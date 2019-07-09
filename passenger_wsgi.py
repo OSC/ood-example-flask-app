9246769cdfda03dee41f7a80f2d5ba1dd05f9723
Samir <mansour.69@osu.edu>
2019-07-09 08:59:13 -0400

    tensorboard

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,42 @@
-from app import MyApp
+import tensorflow as tf
+from tensorboard import program
 
-application = MyApp
+application = None
+
+def run_main():
+  """Initializes flags and calls main()."""
+  tf.app.run(main)
+  
+def main(unused_argv=None):
+  application = program.create_tb_app(default.get_plugins(), default.get_assets_zip_provider())
+
+run_main() 
+
+
+def run_main():
+  """Initializes flags and calls main()."""
+  tf.app.run(main)
+
+
+# def main(unused_argv=None):
+#   """Standard TensorBoard program CLI.
+#   See `tensorboard.program.main` for further documentation.
+#   """
+#   return program.main(default.get_plugins(),
+#             default.get_assets_zip_provider())
+
+
+def main(unused_argv=None):
+  """Standard TensorBoard program CLI.
+  See `tensorboard.program.main` for further documentation.
+  """
+  return program.main(default.get_plugins(),
+            default.get_assets_zip_provider())
+
+
+
+tb = create_tb_app(plugins, assets_zip_provider)
+
 
 if __name__ == '__main__':
-    application.run()
+    run_main() 
